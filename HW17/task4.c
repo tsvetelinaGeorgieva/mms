96d58fc597c29731219d71551e024412710bc549
tsvetelinaGeorgieva <106919111+tsvetelinaGeorgieva@users.noreply.github.com>
2022-06-26 12:55:54 +0300

    Add files via upload

---
+++ HW17/task4.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+
+int main(int argc, char* argv[]){
+    pid_t pid = fork();
+    if(pid == -1){
+        perror("Fork error!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(pid == 0){
+        int err = execlp(argv[1], argv[1], NULL);
+        if(err == -1){
+            perror("execlp error!\n");
+            exit(EXIT_FAILURE);
+        }
+    }else{
+        int wstatus;
+        wait(&wstatus);
+
+        if(WIFEXITED(wstatus)){
+            int statusCode = WEXITSTATUS(wstatus);
+            if(statusCode != 0){
+                wait(NULL);
+                execlp(argv[2], argv[2], NULL);
+                rexit(EXIT_SUCCESS);
+            }else{
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    return 0;
+}
