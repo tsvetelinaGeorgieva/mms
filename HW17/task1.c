d5e982dfc46ba212e5f951eccf00f389431acc3c
tsvetelinaGeorgieva <106919111+tsvetelinaGeorgieva@users.noreply.github.com>
2022-06-26 11:10:37 +0300

    Add files via upload

---
+++ HW17/task1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <time.h>
+
+int randInt(int, int);
+void randomStr(char*);
+
+int main(int argc, char **argv){
+    int N = atoi(argv[1]);
+    for (int i = 0; i < N; i++){
+        pid_t pid = fork();
+        if (-1 == pid){
+            perror("fork");
+            return EXIT_FAILURE;
+        }
+        if (0 == pid){
+            srand(time(NULL) + i);
+            char str[11];
+            randomStr(str);
+            printf("My str is %s\n", str);
+            sleep(1);
+            
+            exit(EXIT_SUCCESS);
+        }
+    }
+    for (int i = 0; i < N; i++){
+        wait(NULL);
+    }
+    return EXIT_SUCCESS;
+}
+
+int randInt(int min, int max){
+    return min + (rand()% (max + 1 - min)); 
+}
+
+void randomStr(char* model){
+    int len = randInt(1, 10);
+    model[0] = randInt('A', 'Z');
+    for(int i = 1; i < len; i++){
+        model[i] = randInt('a', 'z');
+    }
+    model[len] = '\0';
+}
